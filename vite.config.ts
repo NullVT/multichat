9d407d84a7e05c10c6e77231cd1db9767def6bf8
NullVT <nvt@nullvt.com>
2024-07-24 21:34:44 +0100

    fix: set base url

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,16 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
+import { defineConfig } from "vite";
+import vue from "@vitejs/plugin-vue";
 
-// https://vitejs.dev/config/
+/**
+ * https://vitejs.dev/config/
+ */
 export default defineConfig({
+  // no clue why I have to do this despite the docs saying it should be automatic
+  base: process.env.BASE_URL,
+
   plugins: [vue()],
+
   server: {
-    port: 3000
-  }
-})
+    port: 3000,
+  },
+});
