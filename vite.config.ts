53f642f78c4ef8be9dcf68e72d201fcc5ce1a7f4
NullVT <nvt@nullvt.com>
2024-08-10 15:22:08 +0100

    fix: output index ot 404 due to GH pages routing

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,7 @@
 import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
+import { resolve } from "path";
+import { copyFileSync, mkdirSync } from "node:fs";
 
 /**
  * https://vitejs.dev/config/
@@ -8,10 +10,22 @@ export default defineConfig({
   // no clue why I have to do this despite the docs saying it should be automatic
   base: process.env.BASE_URL,
 
-  plugins: [vue()],
-
   server: {
     port: 3000,
     cors: true,
   },
+
+  plugins: [
+    vue(),
+    {
+      name: "copy-index-to-404",
+      closeBundle: () => {
+        const outputDir = resolve(__dirname, "dist");
+        copyFileSync(
+          resolve(outputDir, "index.html"),
+          resolve(outputDir, "404.html")
+        );
+      },
+    },
+  ],
 });
